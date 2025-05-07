28475cedcc020509c8821af1f7d077b0195d8b37
Shi <Shi_D@homehardware.ca>
2025-05-07 11:41:15 -0400

    Adds svelte component

---
+++ src/components/TestSvelte.svelte
@@ -0,0 +1,32 @@
+<script>
+    let word = '';
+  </script>
+  
+<main>
+<label for="wordInput">Enter a word:</label>
+<input id="wordInput" type="text" bind:value={word} />
+
+<p>The word you entered is: {word}</p>
+</main>
+
+<style>
+main {
+    font-family: Arial, sans-serif;
+    padding: 20px;
+}
+
+label {
+    margin-right: 10px;
+}
+
+input {
+    padding: 5px;
+    font-size: 16px;
+}
+
+p {
+    margin-top: 20px;
+    font-size: 18px;
+}
+</style>
+  
